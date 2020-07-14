dd55fd61b7a4c23229abcc998e35de49b9cd3991
Adam Fanelli <afanelli423@gmail.com>
2020-07-14 06:55:02 -0700

    End of Section 2

---
+++ ASP.NET MVC 5 Learning/ASP.NET MVC 5 Learning/OdeToFood.Data/Services/InMemoryRestaurantData.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using OdeToFood.Data.Models;
+using System.Linq;
+
+namespace OdeToFood.Data.Services
+{
+    public class InMemoryRestaurantData : IRestaurantData
+    {
+        List<Restaurant> restaurants;
+
+        public InMemoryRestaurantData()
+        {
+            restaurants = new List<Restaurant>()
+            {
+                new Restaurant { Id = 1, Name = "Belagio's" },
+                new Restaurant { Id = 1, Name = "Red Robin" },
+                new Restaurant { Id = 2, Name = "Buscuit's Cafe" }
+            };
+        }
+
+        public IEnumerable<Restaurant> GetAll()
+        {
+            return restaurants.OrderBy(r => r.Name);
+        }
+    }
+}
