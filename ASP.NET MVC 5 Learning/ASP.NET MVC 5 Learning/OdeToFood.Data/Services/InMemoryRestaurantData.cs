7c749116d6d411aa0b7da33ff0b18d826e5a2c40
Adam Fanelli <afanelli@pointmonitor.com>
2020-07-14 11:14:08 -0700

    Added a responsive bootstrap table for the restaurant data

--- ASP.NET MVC 5 Learning/ASP.NET MVC 5 Learning/OdeToFood.Data/Services/InMemoryRestaurantData.cs
+++ ASP.NET MVC 5 Learning/ASP.NET MVC 5 Learning/OdeToFood.Data/Services/InMemoryRestaurantData.cs
@@ -14,11 +14,11 @@ namespace OdeToFood.Data.Services
             restaurants = new List<Restaurant>()
             {
                 new Restaurant { Id = 1, Name = "Belagio's" },
-                new Restaurant { Id = 1, Name = "Red Robin" },
-                new Restaurant { Id = 2, Name = "Buscuit's Cafe" },
-                new Restaurant { Id = 1, Name = "Belagio's" },
-                new Restaurant { Id = 1, Name = "Red Robin" },
-                new Restaurant { Id = 2, Name = "Buscuit's Cafe" }
+                new Restaurant { Id = 2, Name = "Red Robin" },
+                new Restaurant { Id = 3, Name = "Buscuit's Cafe" },
+                new Restaurant { Id = 4, Name = "Wong's Chinese" },
+                new Restaurant { Id = 5, Name = "Panera Bread" },
+                new Restaurant { Id = 6, Name = "Oswego Grill" }
             };
         }
 
