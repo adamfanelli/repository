4b7f3fa840a53d9aa135f54785ec2ca5aba53d31
Adam Fanelli <afanelli423@gmail.com>
2020-08-29 08:27:08 -0700

    Timeline Initial Commit

---
+++ Timeline/Timeline/Program.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using SFML.Graphics;
+using SFML.Window;
+using SFML.System;
+using TimelineLib;
+
+namespace Timeline
+{
+    class Program
+    {
+        public static RenderWindow window;
+        public static TimelineModel model;
+
+        public static bool KeyPressed_W;
+        public static bool KeyPressed_A;
+        public static bool KeyPressed_S;
+        public static bool KeyPressed_D;
+
+        static void Main(string[] args)
+        {
+            model = new TimelineModel();
+            window = new RenderWindow(new VideoMode(model.WindowWidth, model.WindowHeight), model.WindowTitle);
+
+            //Controls
+
+
+            while (window.IsOpen)
+            {
+                window.DispatchEvents();
+
+                window.Closed += new EventHandler(OnClose);
+                window.KeyPressed += new EventHandler<KeyEventArgs>(myKeyHandler);
+                window.KeyReleased += new EventHandler<KeyEventArgs>(myKeyReleasedHandler);
+                window.Resized += new EventHandler<SizeEventArgs>(myResizeHandler);
+
+                window.Clear(Color.White);
+
+                //DRAW LINE
+                model.DrawLine(window);
+
+                //DRAW MARKERS
+                model.DrawMarkers(window);
+
+
+                window.Display();
+            }
+        }
+
+
+        private static void myResizeHandler(object sender, SizeEventArgs e)
+        {
+            FloatRect visibleArea = new FloatRect(new Vector2f(0, 0), new Vector2f(e.Width, e.Height));
+            window.SetView(new View(visibleArea));
+        }
+
+        private static void myKeyHandler(object sender, KeyEventArgs e)
+        {
+            switch (e.Code)
+            {
+                case Keyboard.Key.W:
+                    if (!KeyPressed_W)
+                    {
+                        model.OffsetY -= model.PanSpeed;
+                        KeyPressed_W = true;
+                    }
+                    break;
+
+                case Keyboard.Key.A:
+                    if (!KeyPressed_A)
+                    {
+                        model.OffsetX -= model.PanSpeed;
+                        KeyPressed_A = true;
+                    }
+                    break;
+
+                case Keyboard.Key.S:
+                    if (!KeyPressed_S)
+                    {
+                        model.OffsetY += model.PanSpeed;
+                        KeyPressed_S = true;
+                    }
+                    break;
+
+                case Keyboard.Key.D:
+                    if (!KeyPressed_D)
+                    {
+                        model.OffsetX += model.PanSpeed;
+                        KeyPressed_D = true;
+                    }
+                    break;
+            }
+        }
+        private static void myKeyReleasedHandler(object sender, KeyEventArgs e)
+        {
+            switch (e.Code)
+            {
+                case Keyboard.Key.W:
+                    KeyPressed_W = false;
+                    break;
+
+                case Keyboard.Key.A:
+                    KeyPressed_A = false;
+                    break;
+
+                case Keyboard.Key.S:
+                    KeyPressed_S = false;
+                    break;
+
+                case Keyboard.Key.D:
+                    KeyPressed_D = false;
+                    break;
+
+
+            }
+        }
+
+        private static void OnClose(object sender, EventArgs e)
+        {
+            window.Close();
+        }
+    }
+}
---
+++ Timeline/TimelineLib/TimelineModel.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using SFML.Graphics;
+using SFML.Window;
+using SFML.System;
+using TimelineLib;
+using System.ComponentModel.DataAnnotations;
+
+namespace TimelineLib
+{
+    public class TimelineModel
+    {
+        public uint WindowWidth { get; set; }
+        public uint WindowHeight{ get; set; }
+        public string WindowTitle { get; set; }
+
+        public RectangleShape Line { get; set; }
+        public float MarkerInterval { get; set; }
+        public float MarkerHeight { get; set; }
+
+        public float OffsetX { get; set; }
+        public float OffsetY { get; set; }
+        public float PanSpeed { get; set; }
+
+        public Font font { get; set; }
+
+        public TimelineModel()
+        {
+            WindowTitle = "Timeline Project";
+            WindowWidth = 1200;
+            WindowHeight = 900;
+
+            OffsetX = 0;
+            OffsetY = 0;
+            PanSpeed = 8f;
+
+            font = new Font("D:\\dev\\repository\\Timeline\\TimelineLib\\Fonts\\JMH Typewriter.ttf");
+
+            Line = new RectangleShape();
+            Line.FillColor = Color.Black;
+            Line.OutlineThickness =  4;
+
+            MarkerInterval = 60;
+            MarkerHeight = 14;
+        }
+
+        public void DrawLine(RenderWindow window)
+        {
+            Line.Position = new Vector2f(0, window.Size.Y / 2);
+            Line.Size = new Vector2f(window.Size.X, Line.OutlineThickness);
+            window.Draw(Line);
+        }
+
+        public void DrawMarkers(RenderWindow window)
+        {
+            int nMarkers = 0;
+            float x = window.Size.X / 2 + OffsetX;
+
+            while (x < window.Size.X)
+            {
+                DrawMarker(window, x);
+                x += MarkerInterval;
+                nMarkers++;
+            }
+
+            x = window.Size.X / 2 + OffsetX - MarkerInterval;
+
+            while (x > 0)
+            {
+                DrawMarker(window, x);
+                x -= MarkerInterval;
+                nMarkers++;
+            }
+
+            //Display num of markers and OffsetX at top of screen for testing
+            Text text = new Text(nMarkers.ToString(), font);
+            text.Position = new Vector2f(window.Size.X - 60, 20);
+            text.FillColor = Color.Black;
+            window.Draw(text);
+            
+            Text text2 = new Text(OffsetX.ToString(), font);
+            text2.Position = new Vector2f(window.Size.X - 60, 80);
+            text2.FillColor = Color.Black;
+            window.Draw(text2);
+        }
+
+        private void DrawMarker(RenderWindow window, float x)
+        {
+            RectangleShape marker = new RectangleShape();
+            marker.Position = new Vector2f(x, Line.Position.Y + Line.OutlineThickness / 2 - MarkerHeight / 2);
+            marker.Size = new Vector2f(Line.OutlineThickness, MarkerHeight);
+            marker.FillColor = Color.Black;
+            window.Draw(marker);
+        }
+    }
+}
